de185f2fad0acf832fc3c618a8bb4e7afddb2231
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2020-06-22 20:03:46 +0900

    JS0622
    Done

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
+const title = document.querySelector("#title");
 
-const hoonInfo = {
-    name:"Hoon",
-    age : 33,
-    gender : "Male",
-    isHandsome: true,
-    favMovies: ["Along the gods","LOTR", "OldBoy"],
-    favFood: [
-        {
-            name:"Kimchi",
-            fatty: false}, 
-        {
-            name:"Cheeseburger",
-            fatty: true}
-    ] //object의 array를 만듦
-}; //data에 lable을 붙여준것인데 해당 데이터 뽑고 싶을때 .하고 lable명 적어주면 됨.
+const CLICKED_CLASS ="clicked";
 
-console.log(hoonInfo.favFood[0].fatty);
+function handleClick() {
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+    title.addEventListener("click", handleClick);
+}
+
+init();
+
